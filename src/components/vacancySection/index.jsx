1ce0a7cf0440bad5eb029dc139759ff7b7f293ce
AminaYassin <amina6012016@gmail.com>
2026-02-09 17:45:03 +0100

    fix last section

--- src/components/vacancySection/index.jsx
+++ src/components/vacancySection/index.jsx
@@ -1,6 +1,7 @@
 import styles from "./styles.module.css";
 import Title from "../title";
 import VacancyCard from "../vacancyCards";
+import Button from "../button";
 
 function Vacancy() {
   return (
@@ -8,6 +9,9 @@ function Vacancy() {
       <Title title={"Новые вакансии"} />
       <p className={styles.text}>Найди работу своей мечты прямо сейчас</p>
       <VacancyCard />
+      <div className={styles.btnFlex}>
+        <Button title={"Все вакансии"} variant={"secondButton"} />
+      </div>
     </section>
   );
 }
