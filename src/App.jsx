d36fceeb0a14c3fa807a6a9d99fa60b5de7699b9
AminaYassin <amina6012016@gmail.com>
2026-02-09 13:56:05 +0100

    done category section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/header";
 import Hero from "./components/heroSection";
 import FindJobSection from "./components/findJobSection";
+import Category from "./components/categorySection";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Hero />
       <FindJobSection />
+      <Category />
     </div>
   );
 }
