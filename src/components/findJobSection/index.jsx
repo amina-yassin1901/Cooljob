e979484f07208e095bba4ecc95ecada4c0cc6942
AminaYassin <amina6012016@gmail.com>
2026-02-09 13:06:32 +0100

    done section

--- src/components/findJobSection/index.jsx
+++ src/components/findJobSection/index.jsx
@@ -6,7 +6,26 @@ import sony from "/src/assets/icons/sony.svg";
 import cocacola from "/src/assets/icons/cocacola.svg";
 
 function FindJobSection() {
-  const companyIcons = [{ id: 1, src: disney, name: "disney" }];
-  return <section></section>;
+  const companyIcons = [
+    { id: 1, src: disney, name: "disney" },
+    { id: 2, src: facebook, name: "facebook" },
+    { id: 3, src: microsoft, name: "microsoft" },
+    { id: 4, src: sony, name: "sony" },
+    { id: 5, src: cocacola, name: "cocacola" },
+  ];
+  return (
+    <section className={styles.container}>
+      <p className={styles.text}>Помогаем найти работу:</p>
+      <ul className={styles.list}>
+        {companyIcons.map((icon) => {
+          return (
+            <li className={styles.item} key={icon.id}>
+              <img className={styles.img} src={icon.src} alt={icon.name} />
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 }
 export default FindJobSection;
